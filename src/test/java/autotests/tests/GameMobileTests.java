0f09954adabe58236fcf667afc9339f9ebfea51d
Mikhail <bulanjova@mail.ru>
2022-09-13 22:21:29 +0300

    add

--- src/test/java/autotests/tests/GameMobileTests.java
+++ src/test/java/autotests/tests/GameMobileTests.java
@@ -22,4 +22,27 @@ public class GameMobileTests extends TestBase {
                 $(AppiumBy.xpath("//android.widget.FrameLayout[@index='0']")).shouldBe(Condition.visible));
     }
 
+    @Test
+    @DisplayName("Проверяем кликабельность кнопки Terms of Use")
+    void clickTermsOfUseTest() {
+        // switchTo().alert().accept();
+        step("Запускаем игру и нажимаем на кнопку Terms of Use ", () ->
+                $(AppiumBy.xpath("//android.widget.Button[@text='OK' and @index='0']")).click());
+                $(AppiumBy.xpath("//android.widget.Button[@text='TERMS OF USE' and @index='0']")).click();
+        step("Проверяем что появился текст Terms of Use  ", () ->
+                $(AppiumBy.xpath("//android.widget.Button[@text='Close' and @index='0']")).click();
+                $(AppiumBy.xpath("//android.widget.TextView[@text='Terms of Use' and @index='0']")).shouldHave(text("Terms of Use")));
+    }
+
+    @Test
+    @DisplayName("Проверяем кликабельность кнопки Privacy Policy")
+    void clickPrivacyPolicyTest() {
+        // switchTo().alert().accept();
+        step("Запускаем игру и нажимаем на кнопку Privacy Policy", () ->
+                $(AppiumBy.xpath("//android.widget.Button[@text='OK' and @index='0']")).click());
+                $(AppiumBy.xpath("//android.widget.Button[@text='PRIVACY POLICY' and @index='1']")).click();
+        step("Проверяем что появился текст Privacy Policy  ", () ->
+                $(AppiumBy.xpath("//android.widget.TextView[@text='Privacy Policy' and @index='0']")).shouldHave(text("Privacy Policy")));
+    }
+
 }
